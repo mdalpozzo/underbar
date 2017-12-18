7a14591ed8ffee76daeb7245fac1de424fb55eb9
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2017-12-18 12:51:33 -0800

    Implements up to '_.uniq'

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,13 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if ( n === undefined) {
+      return array[array.length -1];
+    } else if (n === 0) {
+      return [];
+    } else {
+      return array.slice(-n);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +53,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var i in collection) {
+        iterator(collection[i], i, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,12 +83,30 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filteredElements = [];
+
+    _.each(collection, function(element) {
+      if (test(element)) {
+        filteredElements.push(element);
+      }
+    })
+
+    return filteredElements;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejectedElements = [];
+
+    _.each(collection, function(element) {
+      if (!test(element)) {
+        rejectedElements.push(element);
+      }
+    })
+
+    return rejectedElements;
   };
 
   // Produce a duplicate-free version of the array.
