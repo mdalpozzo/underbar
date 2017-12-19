a634457f547c9b50c5e2f4c4232d0bc056623449
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2017-12-18 19:54:02 -0800

    Implements '_.contains', '_.every', '_.some'

--- src/underbar.js
+++ src/underbar.js
@@ -229,12 +229,31 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+   if (!iterator) {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(isTrue, item) {
+
+      if (isTrue && iterator(item)) {
+        return true;
+      }
+      return false;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (!iterator) {
+      iterator = _.identity;
+    }
+
+    if (_.every(collection, function(item) { return !iterator(item); })) {
+      return false;
+    }
+    return true;
   };
 
 
